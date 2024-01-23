1454acd278b7d2b053320c1e16676abbcafd6701
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-01-23 13:50:47 -0800

    Added MenuItem class

---
+++ CPW219-eCommerceSite/Models/MenuItem.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW219_eCommerceSite.Models
+{
+    /// <summary>
+    /// Represents a single item on a food menu for purchase
+    /// </summary>
+    public class MenuItem
+    {
+        /// <summary>
+        /// The unique identifier for each product
+        /// </summary>
+        [Key]
+        public int MenuItemID { get; set; }
+
+        /// <summary>
+        /// The name of the food item as it appears on the menu
+        /// </summary>
+        [Required]
+        public string MenuItemName { get; set; }
+
+        /// <summary>
+        /// The price of this specific food item
+        /// </summary>
+        [Range(0, int.MaxValue)]        
+        public double MenuItemPrice { get; set; }
+    }
+}
