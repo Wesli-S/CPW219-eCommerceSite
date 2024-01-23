f346339434434ffa64a121953c6cf3cb548e530a
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-01-23 14:53:18 -0800

    Added DB Context class

---
+++ CPW219-eCommerceSite/Data/CafeContext.cs
@@ -0,0 +1,15 @@
+﻿using CPW219_eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CPW219_eCommerceSite.Data
+{
+    public class CafeContext : DbContext
+    {
+        public CafeContext(DbContextOptions<CafeContext>options) : base(options) 
+        { 
+        
+        }
+
+        public DbSet<MenuItem> menuItems { get; set; }
+    }
+}
