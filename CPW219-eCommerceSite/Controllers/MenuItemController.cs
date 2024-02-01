3c45d6ee3b5fa1e6f3a6b118bd289dd310a3e989
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-02-01 13:02:05 -0800

    Create game full functionality, added some styling

--- CPW219-eCommerceSite/Controllers/MenuItemController.cs
+++ CPW219-eCommerceSite/Controllers/MenuItemController.cs
@@ -1,13 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW219_eCommerceSite.Data;
+using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Drawing.Text;
 
 namespace CPW219_eCommerceSite.Controllers
 {
     public class MenuItemController : Controller
     {
+        private readonly MenuItemContext _context;
+        public MenuItemController(MenuItemContext context)
+        {
+                _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create() 
         { 
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(MenuItem menuItem)
+        {
+            if (ModelState.IsValid)
+            {
+                //Add to DB
+                _context.MenuItems.Add(menuItem); //prepares insert
+                _context.SaveChanges();           //executes pending insert
+
+                //Show success message on page
+                ViewData["Message"] = $"{menuItem.MenuItemName} was added successfully!";
+
+                return View();
+            }
+            return View(menuItem);
+        } 
     }
 }
