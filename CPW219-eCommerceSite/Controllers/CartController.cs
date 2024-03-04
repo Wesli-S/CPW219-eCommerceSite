b0f64e59db9fbdda6c35ff713997acc2572b0d22
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-03-04 14:46:01 -0800

    Added shopping cart button to menu

---
+++ CPW219-eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,31 @@
+﻿using CPW219_eCommerceSite.Data;
+using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly MenuItemContext _context;
+
+        public CartController(MenuItemContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            MenuItem? itemToCart = _context.MenuItems.Where(m => m.MenuItemID == id).SingleOrDefault();
+
+            if(itemToCart == null)
+            {
+                TempData["Message"] = "Sorry! This item is no longer available";
+                return RedirectToAction("Index", "MenuItem");
+            }
+
+            //ToDo: Add item to a shopping cart cookie
+            TempData["Message"] = "Item added to cart!";
+            return RedirectToAction("Index", "MenuItem"); ;
+        }
+    }
+}
