d1c855b933218803e654323c081d08e43cffa96e
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-03-04 16:42:21 -0800

    Coded Add to cart, Remove from cart, and added a cart summary page

--- CPW219-eCommerceSite/Controllers/CartController.cs
+++ CPW219-eCommerceSite/Controllers/CartController.cs
@@ -1,12 +1,14 @@
 ﻿using CPW219_eCommerceSite.Data;
 using CPW219_eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CPW219_eCommerceSite.Controllers
 {
     public class CartController : Controller
     {
         private readonly MenuItemContext _context;
+        private const string Cart = "ShoppingCart"; 
 
         public CartController(MenuItemContext context)
         {
@@ -17,15 +19,72 @@ namespace CPW219_eCommerceSite.Controllers
         {
             MenuItem? itemToCart = _context.MenuItems.Where(m => m.MenuItemID == id).SingleOrDefault();
 
-            if(itemToCart == null)
+            if (itemToCart == null)
             {
                 TempData["Message"] = "Sorry! This item is no longer available";
                 return RedirectToAction("Index", "MenuItem");
             }
 
+            CartItemViewModel cartItem = new()//object mapping
+            {
+                ItemId = itemToCart.MenuItemID,
+                ItemName = itemToCart.MenuItemName,
+                ItemPrice = itemToCart.MenuItemPrice
+            };
+
+            List<CartItemViewModel> cartItems = GetExistingCartData();
+            cartItems.Add(cartItem);
+            WriteShoppingCartCookie(cartItems);
             //ToDo: Add item to a shopping cart cookie
             TempData["Message"] = "Item added to cart!";
             return RedirectToAction("Index", "MenuItem"); ;
+        }
+
+        private void WriteShoppingCartCookie(List<CartItemViewModel> cartItems)
+        {
+            string cookieData = JsonConvert.SerializeObject(cartItems);//returns a string
+
+            HttpContext.Response.Cookies.Append(Cart, cookieData, new CookieOptions()
+            {
+                Expires = DateTimeOffset.Now.AddYears(1)
+            });
+        }
+
+        /// <returns>
+        /// Current List of items in the users shopping cart cookie.
+        /// If there is no cookie, an empty list should be returned
+        /// </returns>
+        private List<CartItemViewModel> GetExistingCartData()
+        {
+            string? cookie = HttpContext.Request.Cookies[Cart];
+            if (string.IsNullOrWhiteSpace(cookie))
+            {
+                return new List<CartItemViewModel>();
+            }
+            return JsonConvert.DeserializeObject<List<CartItemViewModel>>(cookie);
+        }
+
+        public IActionResult Summary()
+        {
+            //Read shopping cart data and convert to list of view model
+            List<CartItemViewModel> cartItems = GetExistingCartData();
+            return View(cartItems);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            //Remove specified item from the cookie
+            List<CartItemViewModel> cartItems = GetExistingCartData();
+
+            CartItemViewModel targetItem =
+                cartItems.Where(m => m.ItemId == id).FirstOrDefault();
+
+            cartItems.Remove(targetItem);
+
+            WriteShoppingCartCookie(cartItems);
+
+            return RedirectToAction("Summary");
+
         }
     }
 }
